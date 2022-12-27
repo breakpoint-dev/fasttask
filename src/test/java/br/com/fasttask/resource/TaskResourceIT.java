902696217693dbf6e1733e1b827d3ec20121abb0
Breakpoint Tecnologia <breakpoint.tecnologia@gmail.com>
2022-12-27 17:33:53 -0300

    Base project

---
+++ src/test/java/br/com/fasttask/resource/TaskResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.fasttask.resource;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class TaskResourceIT extends TaskResourceTest {
+    // Execute the same tests but in packaged mode.
+}
